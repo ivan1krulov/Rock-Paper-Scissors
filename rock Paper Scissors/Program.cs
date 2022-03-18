b93370bdb56def87998dcc4a5eecd9ed94ed17cf
Ivan Krylov <50463318+ivan1krulov@users.noreply.github.com>
2022-03-18 17:11:43 +0300

    Add project files.

---
+++ rock Paper Scissors/Program.cs
@@ -0,0 +1,91 @@
+﻿Console.Title = "Камень - Ножницы - Бумага";
+
+int player_point = 0;
+int AI_point = 0;
+int player_choice = 0;
+
+start:
+
+Console.WriteLine("Игра - Камень, Ножницы Бумага");
+Console.WriteLine("Выбирайте ваш супер предмет !!!");
+checkpoint:
+Console.WriteLine("[1] Камень [2] Ножницы [3] Бумага");
+try
+{
+    player_choice = Convert.ToInt32(Console.ReadLine());
+}
+catch
+{
+
+}
+
+switch (player_choice)
+{
+    case 1:
+        Console.WriteLine("Вы выбрали Камень");
+        player_point = 2;
+        break;
+
+    case 2:
+        Console.WriteLine("Вы выбрали Ножницы");
+        player_point = 1;
+        break;
+
+    case 3:
+        Console.WriteLine("Вы выбрали Бумагу");
+        player_point = 0;
+        break;
+
+    default:
+        Console.WriteLine(" ОШИБКА ! Введите номер предмета");
+        goto checkpoint;
+}
+
+Random value = new Random();
+int AI_choice = value.Next(1, 4);
+Console.Clear();
+switch (AI_choice)
+{
+    case 1:
+        Console.WriteLine("Противник выбрал Камень");
+        AI_point = 2;
+        break;
+
+    case 2:
+        Console.WriteLine("Противник выбрал Ножницы");
+        AI_point = 1;
+        break;
+
+    case 3:
+        Console.WriteLine("Противник выбрал Бумагу");
+        AI_point = 0;
+        break;
+}
+
+if (player_point == AI_point)
+{
+    Console.WriteLine("Ничья");
+    goto start;
+}
+
+else if ((player_point == 0) && (AI_point == 2))
+{
+    Console.WriteLine("С победой !");
+    goto start;
+}
+
+else if ((player_point == 2) && (AI_point == 0))
+{
+    Console.WriteLine("Ты проиграл");
+    goto start;
+}
+else if (player_point > AI_point)
+{
+    Console.WriteLine("Ты выиграл");
+    goto start;
+}
+else if (player_point < AI_point)
+{
+    Console.WriteLine("Ты проиграл");
+    goto start;
+}
